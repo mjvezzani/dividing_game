59423b4e91526faa4f57ff600149a83c657176ef
Michael Vezzani <mjvezzani@gmail.com>
2016-01-22 08:12:27 -0500

    Refactor and implement recursion solution

--- lib/dividing_game.rb
+++ lib/dividing_game.rb
@@ -1,17 +1,24 @@
 class DividingGame
-  def self.getNumber(a, b)
+  def getNumber(a, b)
     return 0 if (a.odd? || b.odd?)
 
     a_times, b_times = [], []
-    until( a%2 != 0 )
-      a_times << a
-      a /= 2
-    end
-    until( b%2 != 0 )
-      b_times << b
-      b /= 2
-    end
 
-    (a_times & b_times).length + 1
+    push_and_divide(a_times, a)
+    push_and_divide(b_times, b)
+
+    (a_times & b_times).length
+  end
+
+  private
+
+  # I found help understanding recursion in ruby by reading under the Recursion heading
+  # at: https://launchschool.com/books/ruby/read/loops_iterators
+  def push_and_divide(acc, num)
+    acc << num
+    if num.even?
+      num /= 2
+      push_and_divide(acc, num)
+    end
   end
 end
